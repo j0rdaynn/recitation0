f7018a5da4abfd73cd43a14df13c48a1e4897ab7
j0rdaynn <jordaynn@icloud.com>
2023-01-19 17:26:27 -0500

    Completed

--- src/main/java/Recitation0.java
+++ src/main/java/Recitation0.java
@@ -1,5 +1,8 @@
 public class Recitation0 {
     public static void main(String [] args) {
         System.out.println("Hello World!");
+
+        System.out.println(1/5.0);
+        System.out.println(0.2*5);
     }
 }
